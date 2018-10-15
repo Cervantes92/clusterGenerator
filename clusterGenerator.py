ab469c50f640e71416a89c5af944f4ee2a573e55
Alexander Peterson <petersonalex50@gmail.com>
2018-10-15 08:36:20 -0500

    Transposed results

--- clusterGenerator.py
+++ clusterGenerator.py
@@ -1,5 +1,6 @@
 import random as rnd
 import numpy as np
+import math as mth
 
 #Cluster field metrics
 clustFieldXSize = 10
@@ -29,10 +30,14 @@ for i in range(numberOfClusters):
 for i in range(numberOfClusters):
 	for x in range(numberOfPointsPerCluster):
 		for y in range(numberOfPointsPerCluster):
-			xPoints.append(xCenter[i] + rnd.uniform(-maxDistanceFromCenter,maxDistanceFromCenter))
-			yPoints.append(yCenter[i] + rnd.uniform(-maxDistanceFromCenter,maxDistanceFromCenter))
+			xPoints.append(xCenter[i] + rnd.uniform(-1,1) * maxDistanceFromCenter * mth.cos(2 * mth.pi * rnd.random()))
+			yPoints.append(yCenter[i] + rnd.uniform(-1,1) * maxDistanceFromCenter * mth.sin(2 * mth.pi * rnd.random()))
 
 
 #Set as a 2d array
 a = np.asarray([xPoints, yPoints])
 np.savetxt("clust.csv", a, delimiter = ",")
+
+#Save the centers array
+centArray = np.asarray([xCenter, yCenter])
+np.savetxt("cent.csv", centArray, delimiter = ",")
