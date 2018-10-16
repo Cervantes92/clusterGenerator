1e122aba03fa681060a66fdd52ada585753bf5f5
Alexander Peterson <petersonalex50@gmail.com>
2018-10-16 13:22:27 -0500

    Normal distribution, reworked calculations

--- clusterGenerator.py
+++ clusterGenerator.py
@@ -1,15 +1,16 @@
 import random as rnd
 import numpy as np
 import math as mth
+import matplotlib.pyplot as plt
 
 #Cluster field metrics
-clustFieldXSize = 10
-clustFieldYSize = 10
+clustFieldXSize = 3
+clustFieldYSize = 3
 
 #Cluster metrics
-numberOfClusters = 5
-numberOfPointsPerCluster = 100
-maxDistanceFromCenter = 4
+numberOfClusters = 3
+numberOfPointsPerCluster = 30
+stnDev = 1
 
 #Cluster centers array
 xCenter = []
@@ -19,7 +20,11 @@ yCenter = []
 xPoints = []
 yPoints = []
 
-
+#Create a Gaussian density function
+def nrmlDist(x, mean, stanDev):
+	ampli = (2 * mth.pi * stanDev ** 2) ** (-0.5)
+	expon = -((x - mean) ** 2) / (2 * stanDev ** 2)
+	return ampli * mth.e ** expon
 
 #Generate cluster centers in a region specified by the field metrics
 for i in range(numberOfClusters):
@@ -30,14 +35,18 @@ for i in range(numberOfClusters):
 for i in range(numberOfClusters):
 	for x in range(numberOfPointsPerCluster):
 		for y in range(numberOfPointsPerCluster):
-			xPoints.append(xCenter[i] + rnd.uniform(-1,1) * maxDistanceFromCenter * mth.cos(2 * mth.pi * rnd.random()))
-			yPoints.append(yCenter[i] + rnd.uniform(-1,1) * maxDistanceFromCenter * mth.sin(2 * mth.pi * rnd.random()))
+			theta = rnd.uniform(0,2 * mth.pi)
+			xPoints.append(xCenter[i] + rnd.random() * nrmlDist(0.1, 0, stnDev) * mth.cos(theta))
+			yPoints.append(yCenter[i] + rnd.random() * nrmlDist(0.1, 0, stnDev) * mth.sin(theta))
 
+#Plot the results
+plt.plot(xPoints, yPoints, 'r.')
+plt.show()
 
 #Set as a 2d array
 a = np.asarray([xPoints, yPoints])
-np.savetxt("clust.csv", a, delimiter = ",")
+np.savetxt("clust.csv", np.transpose(a), delimiter = ",")
 
 #Save the centers array
 centArray = np.asarray([xCenter, yCenter])
-np.savetxt("cent.csv", centArray, delimiter = ",")
+np.savetxt("cent.csv", np.transpose(centArray), delimiter = ",")
